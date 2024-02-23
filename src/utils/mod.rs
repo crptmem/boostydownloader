e2810a20e7382de1de650156681557bd2f19dc17
crptmem <corrupt@kijy.de>
2024-02-23 16:49:54 +0300

    fix: warnings

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,4 @@
 use colored::Colorize;
-use rand::Rng;
-use std::iter;
 use std::fs::File;
 use error_chain::error_chain;
 
@@ -9,13 +7,6 @@ error_chain! {
         Io(std::io::Error);
         HttpRequest(reqwest::Error);
     }
-}
-
-pub fn generate(len: usize) -> String {
-    const CHARSET: &[u8] = b"abcdef0123456789";
-    let mut rng = rand::thread_rng();
-    let one_char = || CHARSET[rng.gen_range(0..CHARSET.len())] as char;
-    iter::repeat_with(one_char).take(len).collect()
 }
 
 pub async fn download(url: String, path: String) -> Result<()> {
