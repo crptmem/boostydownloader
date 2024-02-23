0ef5556d1b4c2dd3e08d4c92946336eacdff2a9b
crptmem <corrupt@kijy.de>
2024-02-23 16:47:33 +0300

    feat: authorization, add information about access token

--- README.txt
+++ README.txt
@@ -7,6 +7,15 @@ Build
   $ git clone https://github.com/crptmem/boostydownloader.git && cd boostydownloader
   $ cargo build
 
+Obtaining access token
+======================
+Go to https://boosty.to, open developer tools in your browser,
+go to Storage (Application) -> Cookies. You need `auth`, click on it and in
+right panel click RMB on `accessToken` and copy it.
+
 Usage
 =====
+ Without authorization:
   $ boostydownloader --blog USERNAME
+ With authorization:
+  $ boostydownloader --blog USERNAME --access-token ACCESS_TOKEN
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use boosty_rs::request;
+use boosty_rs::auth::Auth;
 
 use clap::Parser;
 use colored::Colorize;
@@ -13,22 +14,38 @@ struct Args {
     blog: String,
 
     #[arg(short, long)]
-    path: Option<String>
+    path: Option<String>,
+
+    #[arg(short, long)]
+    access_token: Option<String>
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
+    let auth: Option<Auth> = if args.access_token.is_some() { Some(Auth::new(args.access_token.unwrap())) } else { None };
     let path = if args.path.is_some() { args.path.unwrap() } else { format!("img/{}", args.blog.clone()) };
     println!("Downloading all content from {} to {}", args.blog.purple(), path.green());
-    let response = request::fetch_posts(args.blog.clone()).await?;
+    let response = request::fetch_posts(args.blog.clone(), auth.clone()).await?;
     std::fs::create_dir_all(path.clone())?;
     for i in response.iter() {
-        for teaser in &i.teaser {
-            if teaser.url.is_some() {
-                utils::download(
-                    teaser.url.clone().unwrap(),
-                    path.clone()).await?;
+        if auth.is_some() {
+            for data in &i.data { 
+                for content in data.iter() {
+                    if content.url.is_some() && content.url.clone().unwrap().starts_with("https://images.boosty.to/image/") {
+                        utils::download(
+                            content.clone().url.clone().unwrap(),
+                            path.clone()).await?;
+                    }
+                }
+            }
+        } else {
+            for teaser in &i.teaser { 
+                if teaser.url.is_some() {
+                    utils::download(
+                        teaser.url.clone().unwrap(),
+                        path.clone()).await?;
+                }  
             }
         }
     }
