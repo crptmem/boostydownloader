2c611e19c375ee1575bb051ec1d52681fdd9d4f2
crptmem <corrupt@kijy.de>
2024-04-07 12:35:18 +0300

    fix: typo

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -12,7 +12,7 @@ error_chain! {
 pub async fn download(url: String, path: String) -> Result<()> {
     let file = format!("{}/{}.png", path, url.split_once("image/").unwrap().1.split_once("?").unwrap().0);
     if Path::new(&file).exists() {
-        println!("Skipping {} because it already downloaded", file.bright_blue());
+        println!("Skipping {} because it's already downloaded", file.bright_blue());
         return Ok(());
     }
     println!("Downloading from {} to {}", url.bright_blue(), file.green()); 
