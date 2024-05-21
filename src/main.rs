c5976cc03c19e0edac65244ae6812915e411efd0
crptmem <corrupt@kijy.de>
2024-05-21 16:54:33 +0300

    feat: add support for downloading videos uploaded to boosty, bump boosty-rs to 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     let auth: Option<Auth> = if args.access_token.is_some() { Some(Auth::new(args.access_token.unwrap())) } else { None };
     let path = if args.path.is_some() { args.path.unwrap() } else { format!("img/{}", args.blog.clone()) };
-    println!("Downloading all content from {} to {}", args.blog.purple(), path.green());
+    println!("Downloading all pictures from {} to {}", args.blog.purple(), path.green());
     let response = request::fetch_posts(args.blog.clone(), auth.clone()).await?;
     std::fs::create_dir_all(path.clone())?;
     for i in response.iter() {
-        if auth.is_some() {
             for data in &i.data { 
                 for content in data.iter() {
                     if content.url.is_some() && content.url.clone().unwrap().starts_with("https://images.boosty.to/image/") {
@@ -37,9 +36,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             content.clone().url.clone().unwrap(),
                             path.clone()).await?;
                     }
+                    if content.url.is_some() && content.content_type.clone() == "ok_video" {
+                        if content.player_urls.is_some() {
+                            let player_urls = content.player_urls.clone().unwrap();
+                            for player in player_urls {
+                                if player.content_type == "hd" || player.content_type == "full_hd" || player.content_type == "low" {
+                                    utils::download_video(
+                                        player.clone().url, path.clone()).await?;
+                                    break;
+                                }
+                            } 
+                        }
+                    }
                 }
             }
-        } else {
             for teaser in &i.teaser { 
                 if teaser.url.is_some() {
                     utils::download(
@@ -47,8 +57,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         path.clone()).await?;
                 }  
             }
-        }
     }
-    
+
+
     Ok(())
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,7 @@
 use colored::Colorize;
-use std::{fs::File, path::Path};
+use std::{borrow::Borrow, fmt::format, fs::File, path::{Path, PathBuf}};
 use error_chain::error_chain;
+//use ytd_rs::{YoutubeDL, Arg};
 
 error_chain! {
     foreign_links {
@@ -11,6 +12,20 @@ error_chain! {
 
 pub async fn download(url: String, path: String) -> Result<()> {
     let file = format!("{}/{}.png", path, url.split_once("image/").unwrap().1.split_once("?").unwrap().0);
+    if Path::new(&file).exists() {
+        println!("Skipping {} because it's already downloaded", file.bright_blue());
+        return Ok(());
+    }
+    println!("Downloading from {} to {}", url.bright_blue(), file.green()); 
+    let resp = reqwest::get(url).await.expect("request failed");
+    let mut out = File::create(file).expect("failed to create file");
+    std::io::copy(&mut resp.bytes().await.unwrap().as_ref(), &mut out).expect("failed to copy content");
+
+    Ok(())
+}
+
+pub async fn download_video(url: String, path: String) -> Result<()> {
+    let file = format!("{}/{}.mp4", path, url.split_once("&id=").unwrap().1);
     if Path::new(&file).exists() {
         println!("Skipping {} because it's already downloaded", file.bright_blue());
         return Ok(());
