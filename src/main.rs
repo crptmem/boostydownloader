52376feb0fc8296e7792ecf72b7f3dbbb8e0ae30
crptmem <corrupt@kijy.de>
2024-07-10 18:01:38 +0300

    ✨ feat: add gelbooru as an option to download, update cli

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,113 @@
 use std::error::Error;
-use boosty_rs::request;
-use boosty_rs::auth::Auth;
+use imgdl_rs::boosty::auth::Auth;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use colored::Colorize;
 
 mod utils;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+/// Args
 struct Args {
-    #[arg(short, long)]
-    blog: String,
+    #[command(subcommand)]
+    cmd: Commands
+}
 
-    #[arg(short, long)]
-    path: Option<String>,
+/// Subcommands
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    /// Subcommand to download from boosty
+    Boosty {
+        #[arg(short, long)]
+        /// Boosty blog
+        blog: String,
 
-    #[arg(short, long)]
-    access_token: Option<String>,
+        #[arg(short, long)]
+        #[clap(default_value_t = String::from("img"))]
+        /// Path where images will be saved
+        path: String,
 
-    #[arg(short, long)]
-    #[clap(default_value_t = 300)]
-    limit: i64,
+        #[arg(short, long)]
+        /// Boosty access token
+        access_token: Option<String>,
 
-    #[arg(short, long)]
-    #[clap(default_value_t = false)]
-    debug: bool
+        #[arg(short, long)]
+        #[clap(default_value_t = 300)]
+        /// Set limit of maximum images to download
+        limit: i64,
+    },
+    /// Subcommand to download from Gelbooru-powered site
+    Gelbooru {
+        #[arg(short, long)]
+        /// Site URL
+        url: String,
+
+        #[arg(short, long)]
+        #[clap(default_value_t = String::from("img"))]
+        /// Path where images will be saved
+        path: String,
+
+        #[arg(short, long)]
+        /// Gelbooru tags
+        tags: String,
+
+        #[arg(long)]
+        #[clap(default_value_t = 0)]
+        /// Page id
+        pid: i64,
+
+        #[arg(long)]
+        /// Proxy if requested resource is blocked in your country (SOCKS or HTTP)
+        proxy: Option<String>
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let auth: Option<Auth> = if args.access_token.is_some() { Some(Auth::new(args.access_token.unwrap())) } else { None };
-    let path = if args.path.is_some() { args.path.unwrap() } else { format!("img/{}", args.blog.clone()) };
-    println!("Downloading all pictures from {} to {}", args.blog.purple(), path.green());
-    let response = request::fetch_posts(args.blog.clone(), args.limit, auth.clone()).await?;
-    println!("Total count: {}", response.len());
-
-    if args.debug {
-        let response_raw = request::fetch_posts_raw(args.blog.clone(), args.limit, auth.clone()).await?;
-        println!("API response: {:#?}", response_raw);
+    match args.cmd {
+        Commands::Boosty { blog, path, access_token, limit } => {
+            download_boosty(blog, path, access_token, limit).await;
+        },
+        Commands::Gelbooru { url, path, tags, pid, proxy } => {
+            if let Some(proxy_url) = proxy {
+                let proxy_reqwest = reqwest::Proxy::all(proxy_url).unwrap();
+                download_gelbooru(url, tags, pid, path, Some(proxy_reqwest)).await;
+            } else {
+                download_gelbooru(url, tags, pid, path, None).await;
+            }
+           
+        }
     }
 
-    std::fs::create_dir_all(path.clone())?;
+    Ok(())
+}
+
+async fn download_gelbooru(url: String, tags: String, pid: i64, path: String, proxy: Option<reqwest::Proxy>) {
+    println!("Downloading all pictures from tags {} to {}", tags.purple(), path.green());
+    let response = imgdl_rs::gelbooru::request::fetch_posts(url.clone(), Some(tags.clone()), Some(pid), proxy.clone()).await.unwrap();
+    for i in response.iter() {
+        let download_url = format!("{url}/images/{}/{}", i.directory, i.image);
+        utils::download_img_gelbooru(
+            download_url, i.image.clone(), format!("{path}/{tags}/"), proxy.clone()).await.unwrap();
+    }
+}
+
+async fn download_boosty(blog: String, path: String, access_token: Option<String>, limit: i64) {
+    let auth = access_token.map(Auth::new);
+    println!("Downloading all pictures from {} to {}", blog.purple(), path.green());
+    let response = imgdl_rs::boosty::request::fetch_posts(blog.clone(), limit, auth.clone()).await.unwrap();
+    println!("Total count: {}, limit: {}", response.len(), limit);
+
+    std::fs::create_dir_all(path.clone()).unwrap();
     for i in response.iter() {
         if let Some(data) = i.data.clone() { 
             for content in data.iter() {
                 if content.url.is_some() && content.url.clone().unwrap().starts_with("https://images.boosty.to/image/") {
-                    utils::download(
+                    utils::download_img_boosty(
                         content.clone().url.clone().unwrap(),
-                        path.clone()).await?;
+                        path.clone()).await.unwrap();
                 }
                 if content.url.is_some() && content.content_type.clone() == "ok_video"
                     && content.url.clone().unwrap().contains("id") && content.player_urls.is_some() {
@@ -57,7 +115,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         for player in player_urls {
                             if player.content_type == "hd" || player.content_type == "full_hd" || player.content_type == "low" {
                                 utils::download_video(
-                                    player.clone().url, path.clone()).await?;
+                                    player.clone().url, path.clone()).await.unwrap();
                                 break;
                             }
                     } 
@@ -66,13 +124,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
         for teaser in &i.teaser { 
             if teaser.url.is_some() {
-                utils::download(
+                utils::download_img_boosty(
                     teaser.url.clone().unwrap(),
-                    path.clone()).await?;
+                    path.clone()).await.unwrap();
             }  
         }
     }
-
-
-    Ok(())
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,5 @@
 use colored::Colorize;
-use std::{fs::File, path::Path};
+use std::{fs::{self, File}, path::Path};
 use error_chain::error_chain;
 
 error_chain! {
@@ -9,7 +9,35 @@ error_chain! {
     }
 }
 
-pub async fn download(url: String, path: String) -> Result<()> {
+pub async fn download_img_gelbooru(url: String, img_name: String, path: String, proxy: Option<reqwest::Proxy>) -> Result<()> {
+    let file = format!("{path}/{img_name}");
+    if Path::new(&file).exists() {
+        println!("Skipping {} because it's already downloaded", file.bright_blue());
+        return Ok(());
+    }
+    println!("Downloading from {} to {}", url.bright_blue(), file.green());
+    if !Path::new(&path).exists() {
+        fs::create_dir(path).unwrap()
+    }
+    if let Some(proxy_reqwest) = proxy {
+        let client = reqwest::Client::builder()
+            .proxy(proxy_reqwest)
+            .build()?;
+        let resp = client.get(url).send().await.expect("request failed");
+        let mut out = File::create(file).expect("failed to create file");
+        std::io::copy(&mut resp.bytes().await.unwrap().as_ref(), &mut out).expect("failed to copy content");
+
+        Ok(())
+    } else {
+        let resp = reqwest::get(url).await.expect("request failed");
+        let mut out = File::create(file).expect("failed to create file");
+        std::io::copy(&mut resp.bytes().await.unwrap().as_ref(), &mut out).expect("failed to copy content");
+
+        Ok(())
+    }
+}
+
+pub async fn download_img_boosty(url: String, path: String) -> Result<()> {
     let file: String = if url.contains("?") {
         format!("{}/{}.png", path, url.split_once("image/").unwrap().1.split_once("?").unwrap().0)
     } else {
